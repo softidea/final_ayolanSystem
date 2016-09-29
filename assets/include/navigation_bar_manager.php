216ae66008abbfd742de712aac8f73dd05d27185
deepalsuranga <deepalsuranga@live.com>
2016-09-29 10:56:14 +0530

    invoice address auto detected

--- assets/include/navigation_bar_manager.php
+++ assets/include/navigation_bar_manager.php
@@ -13,7 +13,7 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="../user/user_home.php" style="text-decoration: none;">Home</a></li>
+                    <li><a href="<?php echo('../user/manager_home.php'); ?>" style="text-decoration: none;">Home</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Customer<b class="caret"></b></a>
                         <ul class="dropdown-menu">
