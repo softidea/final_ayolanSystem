59d6d6543847b2786e02700c63f39173c888e376
deepalsuranga <deepalsuranga@live.com>
2016-09-29 13:14:23 +0530

    invoice address auto detected

--- customer/report_template_invoice.php
+++ customer/report_template_invoice.php
@@ -4,48 +4,85 @@ session_start();
 if (!isset($_SESSION['user_email'])) {
     header("Location:../index.php");
 }
+
+require_once '../db/mysqliConnect.php';
 ?>
 <html>
     <head>
-        <title>Report | Visit</title>
+        <title>Report | Invoice</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-<?php
 
-$p_vehicle_number="None";
-$p_service_number="None";
-$p_cus_name="None";
-$p_cus_nic="None";
-$p_visit_cost="0.00";
-$p_amount="0.00";
-$p_due_amount="0.00";
-$p_amount_word="None";
+        <?php
+        $p_vehicle_number = "None";
+        $p_service_number = "None";
+        $p_cus_name = "None";
+        $p_cus_nic = "None";
+        $p_visit_cost = "0.00";
+        $p_amount = "0.00";
+        $p_due_amount = "0.00";
+        $p_due_balance = "0.00";
+        $p_amount_word = "None";
 
-if (isset($_SESSION['user_username'])) {
-    $p_username=$_SESSION['user_username'];
-}
+        if (isset($_SESSION['user_username'])) {
+            $p_username = $_SESSION['user_username'];
 
-if ($_SESSION['user_branch']=="Horana") {
-    $a_1="Head Office";
-    $a_2="No: 141";
-    $a_3="Rathnapura Rd";
-    $a_4="Horana";
-    
-    
-}else if($_SESSION['user_branch']=="Bulathsinghala"){
-    $a_1="Branch";
-    $a_2="No: 18";
-    $a_3="Hoarana Rd";
-    $a_4="Bulathsinhale";
-    
-}
+            if ($_SESSION['user_branch'] == "Horana") {
+                $a_1 = "Head Office";
+                $a_2 = "No: 141";
+                $a_3 = "Rathnapura Rd";
+                $a_4 = "Horana";
+            } else if ($_SESSION['user_branch'] == "Bulathsinghala") {
+                $a_1 = "Branch";
+                $a_2 = "No: 18";
+                $a_3 = "Hoarana Rd";
+                $a_4 = "Bulathsinhale";
+            }
+        }
 
-?>
+        $p_invNumber = "";
+
+        $qy_getInvoiceNumber = "SELECT inv_aid FROM invoice ORDER BY inv_aid DESC LIMIT 1";
+        $result = mysqli_query($conn, $qy_getInvoiceNumber);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                $p_invNumber = $row["inv_aid"];
+                echo '<html><script>alert(' . $p_invNumber . ');</script></html>';
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $p_invNumber = $p_invNumber + 1;
+        $lenth_in = strlen($p_invNumber);
+        $p_NinvNumber = "";
+        if ($lenth_in == 0) {
+            $p_NinvNumber = "IN-00001";
+        } else if ($lenth_in == 1) {
+            $p_NinvNumber = "IN-0000" . $p_invNumber;
+        } else if ($lenth_in == 2) {
+            $p_NinvNumber = "IN-000" . $p_invNumber;
+        } else if ($lenth_in == 3) {
+            $p_NinvNumber = "IN-00" . $p_invNumber;
+        } else if ($lenth_in == 4) {
+            $p_NinvNumber = "IN-0" . $p_invNumber;
+        } else if ($lenth_in == 5) {
+            $p_NinvNumber = "IN-" . $p_invNumber;
+        }
+
+        $user_name = $_SESSION['user_username'];
+        date_default_timezone_set('Asia/Colombo');
+        $paid_date = date("Y-m-d");
+
+        $qy_invoiceAdd = "INSERT INTO invoice (inv_number,inv_issue_user,inv_ser_number,inv_due_amount,inv_paid_amount,inv_date_time) VALUES('$p_NinvNumber','" . $user_name . "','$p_due_amount','$p_amount','$paid_date')";
+
+        mysqli_query($d_bc, $qy_invoiceAdd);
+        ?>
 
 
-        
-        
+
+
     </head>
     <body>
         <style>
@@ -157,7 +194,7 @@ if ($_SESSION['user_branch']=="Horana") {
 
                 <h1 style="font-family: sans-serif,Tahoma, Verdana, Segoe">VISIT INVOICE | AYOLAN INVESTMENTS </h1>
                 <hr/>
-                
+
                 <table style="width: 300px;float:right;font-family: sans-serif,Tahoma, Verdana, Segoe;">
                     <tr>
                         <td style="width:50px;text-align: right;">
@@ -176,18 +213,18 @@ if ($_SESSION['user_branch']=="Horana") {
                                 +94 034 22 65 107
                             </address>
                         </td>
-                       
+
                     </tr>
                 </table>
             </header>
             <hr style="padding-top: 0px;margin: 0px;"/>
-            
-                <div style="float: right;">
-                    
-                     Date :<?php echo date("d-m-Y"); ?>
-                </div>
-                <br/>
-                <table>
+
+            <div style="float: right;">
+
+                Date :<?php echo date("d-m-Y"); ?>
+            </div>
+            <br/>
+            <table>
                 <tr>
                     <td style="width:200px;">Vehicle No</td>
                     <td>:<?php echo "$p_vehicle_number"; ?></td>
@@ -207,33 +244,33 @@ if ($_SESSION['user_branch']=="Horana") {
                     <td>:<?php echo "$p_amount_word"; ?></td>
                 </tr>
             </table>
-           
 
-                <table style="float: right;height: 40px;">
-                    <tr>
-                        <td style="width:200px;"><strong>Amount</strong></td>
-                        <td>:<strong><?php echo "$p_amount"; ?></strong></td>
-                    </tr>
-                    
-                    <tr>
-                        <td style="width:200px;"><strong>Due Amount</strong></td>
-                        <td>:<strong><?php echo "$p_due_amount"; ?></strong></td>
-                    </tr>
-                    
-                    <tr>
-                        <td style="width:200px;"><strong>Due Amount</strong></td>
-                        <td><strong>105000.00</strong></td>
-                    </tr>
-                </table>
+
+            <table style="float: right;height: 40px;">
+                <tr>
+                    <td style="width:200px;"><strong>Amount</strong></td>
+                    <td>:<strong><?php echo "$p_amount"; ?></strong></td>
+                </tr>
+
+                <tr>
+                    <td style="width:200px;"><strong>Due Amount</strong></td>
+                    <td>:<strong><?php echo "$p_due_amount"; ?></strong></td>
+                </tr>
+
+                <tr>
+                    <td style="width:200px;"><strong>Due Amount</strong></td>
+                    <td><strong><?php echo "$p_due_balance"; ?></strong></td>
+                </tr>
+            </table>
 
             <br/>
             <br/>
-           
+
             <br/>
 
             <hr/>
             <div style="width: 100%;">
-                <p>Customer Signature:..................................<span><br/></span> Officer Signature:..................................(<?php echo "$p_username";  ?>)</p>
+                <p>Customer Signature:..................................<span><br/></span> Officer Signature:..................................(<?php echo "$p_username"; ?>)</p>
             </div>
             <hr/>
 
